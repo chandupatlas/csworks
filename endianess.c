dcd67d7055b06869e3bbc64281cf095869e40090
Chris <srinivas.chandupatla@gmail.com>
2017-05-04 15:55:14 +0530

    Create endianess.c

---
+++ endianess.c
@@ -0,0 +1,22 @@
+/* Description :  Check processors in little-endian or big-endian
+ * Author      : Srinivas Chandupatla
+ * Date        : 24.03.2008
+ * File        : endianess.c
+ * Compile     : gcc -Wall endianess -o endianess
+ * Execute     : ./endianess
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+   int data = 1;
+
+   if (1 == *(char *)&data)
+      printf("--> CPU is LITTLE ENDIAN <--\n");
+   else
+      printf("--< CPU is BIG ENDIAN >--\n");
+
+   return EXIT_SUCCESS;
+}
